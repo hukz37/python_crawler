aad7c7fa860a52b7f63368d1241890c65b82f48f
hukezhu <HUKEZHU098@pingan.com.cn>
2018-07-18 19:21:06 +0800

    shici

--- shici.py
+++ shici.py
@@ -22,7 +22,24 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
     'referer':'https://so.gushiwen.org/gushi/tangshi.aspx'
 }
+db = MySQLdb.connect(host='127.0.0.1',user='root',passwd='123456',db='POEM',charset='utf8')
+cursor = db.cursor()
 
+# cursor.execute("DROP TABLE IF EXISTS POEM")
+#
+# #sql = """CREATE TABLE YUNGAO11 {ID INT NOT NULL,name CHAR(200),price CHAR(200),web CHAR(200)}"""
+# sql = """CREATE TABLE POEM (
+#         ID INT NOT NULL,
+#         category_name  text,
+#         category_url  text,
+#         categoryName text,
+#         title text,
+#         author text,
+#         poemurl text,
+#         poem text,
+#         yiwen text)"""
+#
+# cursor.execute(sql)
 
 def get_id(url):
     #print url
@@ -38,7 +55,7 @@ def get_id(url):
             print info.text + '   ' + 'https://so.gushiwen.org' + info.get('href')
             # try:
             # db.set_character_set('utf8')
-            get_comment_info("https://so.gushiwen.org"+info.xpath('@href')[0])
+            get_comment_info(info.text,"https://so.gushiwen.org"+info.xpath('@href')[0])
         except IndexError:
             pass
 
@@ -46,8 +63,10 @@ def get_id(url):
 
 
 
-def get_comment_info(url):
+def get_comment_info(name,url):
     #print url
+    category_name = name
+    category_url = url
     html = requests.get(url)
     #print html.text
     selector = etree.HTML(html.text)
@@ -63,19 +82,32 @@ def get_comment_info(url):
                 author = titleinfo.xpath('text()')[0]
                 url = "https://so.gushiwen.org%s" % titleinfo.xpath('a')[0].get('href')
                 print "%s  %s  %s" % (title,author,url)
-                get_poem_detail(title,author,url)
+                get_poem_detail(category_name,category_url,categoryName,title,author,url)
         except IndexError:
             pass
 
         time.sleep(2)
 
-def get_poem_detail(title,author,url):
-    html = requests.get(url)
+def get_poem_detail(category_name,category_url,categoryName,title,author,poemurl):
+    html = requests.get(poemurl)
     selector = etree.HTML(html.text)
     source = selector.xpath('//p[@class="source"]/a[1]/text()')[0] #唐代
     poem = selector.xpath('//div[@class="contson"]')[0].xpath('string(.)') #//*[@id="contson45c396367f59"]/text()[2]  //*[@id="contson45c396367f59"]/text()[2]
     poem = MySQLdb.escape_string(poem)
-    print poem
+
+    list = selector.xpath('//div[@class="contyishang"]')
+    for node in list:
+        print node.xpath('string(.)')
+        yiwen = node.xpath('string(.)')
+        yiwen = MySQLdb.escape_string(yiwen)
+        sql1 = "INSERT INTO POEM(category_name,category_url, categoryName,title,author,poemurl,poem,yiwen) \
+                                          VALUES ('%s', '%s', '%s','%s','%s','%s' ,'%s','%s')" % \
+               (category_name, category_url, categoryName, title, author,poemurl,poem,yiwen)
+        cursor.execute(sql1)
+    db.commit()
+
+
+
 
 if __name__ == '__main__':
     url = 'https://so.gushiwen.org/gushi/tangshi.aspx'
